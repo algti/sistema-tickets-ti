55370a5337c441168a630476b9d0d33541ffba7c
algti <contato@algti.com>
2025-09-09 09:57:53 -0300

    schemas.py

--- backend/app/schemas/schemas.py
+++ backend/app/schemas/schemas.py
@@ -14,7 +14,7 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     password: Optional[str] = None
-    role: UserRole = UserRole.USER
+    role: UserRole = UserRole.user
 
 class UserUpdate(BaseModel):
     email: Optional[str] = None
