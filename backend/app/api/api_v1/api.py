91dd83b84ae11405fff918bc1567e7fb7cd62288
algti <contato@algti.com>
2025-09-10 12:45:35 -0300

    api.py

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -4,9 +4,9 @@ from app.api.api_v1.endpoints import auth, tickets, users, categories, dashboard
 
 api_router = APIRouter()
 
+api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
 api_router.include_router(tickets.router, prefix="/tickets", tags=["tickets"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(categories.router, prefix="/categories", tags=["categories"])
 api_router.include_router(dashboard.router, prefix="/dashboard", tags=["dashboard"])
 api_router.include_router(settings.router, prefix="/settings", tags=["settings"])
